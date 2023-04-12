b1e4bc32b5529d593f5f28a7ccfe68271f3f5cf3
Charles Henville <henvillecharles@gmail.com>
2023-04-12 00:40:23 -0400

    Final Menu Changes

---
+++ src/MenuBar.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import styles from './menu.module.css';
+
+function MenuBar() {
+
+    const gitmainurl = "https://cdn-icons-png.flaticon.com/512/25/25231.png";
+    return (
+        <div className={styles.container}>
+
+            <div className={styles.grid}>
+
+                <h1 className={styles.txt}>SunLight Demonstration</h1>
+                <a className={styles.git} target="_blank" href="https://github.com/charleshenville/ocad-interactive-arduino-reactjs">
+                    <img src={gitmainurl} alt="GitHub" className={styles.gitimg}></img>
+                </a>
+            </div>
+
+        </div>
+    );
+}
+
+export default MenuBar;
